a3c2905b48e29cea5b30c6a913745e815c2a142f
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-02-25 00:20:44 +0530

    User.java

---
+++ .src|main|java/.org.SRK.UserApp.dto/User.java
@@ -0,0 +1,46 @@
+package org.SRK.UserApp.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name,mail,password;
+	private long phon;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getPhon() {
+		return phon;
+	}
+	public void setPhon(long phon) {
+		this.phon = phon;
+	}
+}
